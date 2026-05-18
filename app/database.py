76166aba805dde7bf52e68cc7cc2a2f296179c0b
Sayan-Mondal2022 <sayanmondal5678@gmail.com>
2026-05-18 13:51:29 +0530

    Changed the DB connection from Local

--- app/database.py
+++ app/database.py
@@ -2,20 +2,31 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 load_dotenv()
 
-USER_NAME = os.getenv("USER_NAME")
-PASSWORD = os.getenv("PASSWORD")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
+# For local DB setup MySQL
+# USER_NAME = os.getenv("USER_NAME")
+# PASSWORD = os.getenv("PASSWORD")
+# DATABASE_NAME = os.getenv("DATABASE_NAME")
+# DATABASE_URL = f"mysql+pymysql://{USER_NAME}:{PASSWORD}@localhost/{DATABASE_NAME}"
 
-DATABASE_URL = f"mysql+pymysql://{USER_NAME}:{PASSWORD}@localhost/{DATABASE_NAME}"
+# For Deployment
+DATABASE_URL = os.getenv("POSTGRESQL_CONNECTION")
+
 
 # DEBUGGING STATEMENT TO TEST THE URL
 # print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# ASYNCHRONOUS
+# DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+# engine = create_async_engine(DATABASE_URL, echo=True)
+# AsyncSessionLocal = sessionmaker(
+#     bind=engine, class_=AsyncSession, expire_on_commit=False
+# )
 
 Base = declarative_base()
